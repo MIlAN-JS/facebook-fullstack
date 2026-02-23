2d57d809cf7a106fed6014930d8f10554e5e1992
milan-js <milantamang350359@gmail.com>
2026-02-23 18:36:11 +0545

    feat : Register user api created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,14 @@
-const express = require("express")
+const express = require("express");
+const authRouter = require("./routes/auth.routes");
 
 const app = express()
 
+// using middlewares
+app.use(express.json())
 
 
 
-
-
+app.use("/api/auth" , authRouter)
 
 
 module.exports = app;
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+
+
+const userSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        required: [true , "please provide userName"],
+        unique : [true , 'username must be unique']
+    }, 
+    email : {
+        type : String,
+        required: [true , "please provide email"],
+        unique : [true , 'email must be unique']
+    }, 
+    password : {
+        type : String,
+        required: [true , "please provide email"],
+        
+    }, 
+    imgLink : {
+        type : String , 
+        default : ""
+    }
+
+    
+})
+
+
+const userModel = mongoose.model("user", userSchema)
+
+
+module.exports = userModel
