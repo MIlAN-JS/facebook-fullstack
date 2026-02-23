8c9c8e465839d1725768c94b295860233bc36b2e
user.name : "milan-js <user.email : "milantamang350359@gmail.com>
2026-02-24 02:37:33 +0545

    basic frontend layout created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Outlet} from "react-router-dom"
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <main className=''>
+        <Outlet/>
+        <Footer/>
+
+    </main>
+  )
+}
+
+export default App
